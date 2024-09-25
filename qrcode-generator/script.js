226822f5534bed1b320ec9429d2a6038ff2e0d32
pedronns <133705919+pedronns@users.noreply.github.com>
2024-09-25 13:15:32 -0300

    Add files via upload

---
+++ qrcode-generator/script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.container')
+const qrCodeBtn = document.querySelector('#qr-form button')
+
+const qrCodeInput = document.querySelector('#qr-form input')
+
+const qrCodeImg = document.querySelector('#qr-code img')
+
+// eventos
+function generateQrCode () {
+    const qrCodeInputValue = qrCodeInput.value
+    
+    if(!qrCodeInputValue) return
+
+    qrCodeBtn.innerText = 'Gerando código...'
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
+    
+    qrCodeImg.addEventListener('load', () => {
+        container.classList.add('active')
+        qrCodeBtn.innerText = 'Código criado'
+    })
+}
+
+qrCodeBtn.addEventListener('click',  () => {
+    generateQrCode()
+})
